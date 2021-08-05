91cadcc947db158f33b5c8b37d2214c07745c0de
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-05 22:57:08 +0530

    [Daxil] Added:My First Servlet

---
+++ src/main/java/com/bridgelabz/servlet/FirstServlet.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// Annotation used to declare a servlet. 
+@WebServlet("/FirstServlet")
+
+public class FirstServlet extends HttpServlet {
+
+	// Method to allow a servlet to handle a GET request.
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Hello World Daxil My First Servlet!</h3>");
+		out.close();
+	}
+}
